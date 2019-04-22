be9bf85971db52d624097022ed053eafb593bdd7
Sergei Basharov <sergei@basharov.net>
2019-04-22 19:37:53 +0300

    permissions fixes

--- deploy/install-deps.sh
+++ deploy/install-deps.sh
@@ -5,5 +5,5 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | b
 nvm install 10.15.3
 
 cd /home/ec2-user/chink
-
+sudo chown -R ec2-user:ec2-user /home/ec2-user/chink
 npm install
