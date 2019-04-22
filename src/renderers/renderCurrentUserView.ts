adb0796057a4c4b994a82aee39d9cba798c049e7
Sergei Basharov <sergei@basharov.net>
2019-04-22 12:32:39 +0300

    Added logic to clone a private repo, removed ejs

--- src/renderers/renderCurrentUserView.ts
+++ src/renderers/renderCurrentUserView.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express'
 import { getUser } from '../db/getUser'
 import { loadRepos } from '../loadRepos'
+import { cloneRepo } from '../git/cloneRepo'
 
 export const renderCurrentUserView = async (req: Request, res: Response) => {
 
     const user = await getUser(req.user.profile.github_user_id)
 
     const repos = await loadRepos(req.user.accessToken)
+
+    cloneRepo(req.user.accessToken, req.user.profile.github_profile.username, 'testrepo_private')
 
     res.render('profile', {user, repos})
 
