c255f02a6ec53f025f28a5dab0ed33350533631f
Sergei Basharov <sergei@basharov.net>
2019-04-22 19:03:45 +0300

    permissions fixes

--- deploy/install-nvm.sh
+++ deploy/install-nvm.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+source /home/ec2-user/.bash_profile
 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 10.15.3
 
--- deploy/npm-start.sh
+++ deploy/npm-start.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+source /home/ec2-user/.bash_profile
+
 cd /home/ec2-user/chink
 ts-node -r dotenv/config src/server/startApp.ts
