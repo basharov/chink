bb38a279b4a529c8728cecb4184a8f9989a732b6
Sergei Basharov <sergei@basharov.net>
2019-04-22 20:58:26 +0300

    permissions fixes

--- deploy/set-permissions.sh
+++ deploy/set-permissions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Set ownership for all folders
-chown -R ec2-user:ec2-user /home/ec2-user/chink
+#chown -R ec2-user:ec2-user /home/ec2-user/chink
 
 cd /home/ec2-user/chink
 npm install
 
 # set files to 644 [except *.pl *.cgi *.sh]
-find /home/ec2-user/chink -type f -not -name ".pl" -not -name ".cgi" -not -name "*.sh" -print0 | xargs -0 chmod 0644
+#find /home/ec2-user/chink -type f -not -name ".pl" -not -name ".cgi" -not -name "*.sh" -print0 | xargs -0 chmod 0644
 
 # set folders to 755
-find /home/ec2-user/chink -type d -print0 | xargs -0 chmod 0755
+#find /home/ec2-user/chink -type d -print0 | xargs -0 chmod 0755
