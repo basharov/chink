d549f8637fb2b7bfaf953c9c74900976f3412c51
Sergei Basharov <sergei@basharov.net>
2019-04-22 20:54:18 +0300

    permissions fixes

--- deploy/set-permissions.sh
+++ deploy/set-permissions.sh
@@ -2,6 +2,9 @@
 # Set ownership for all folders
 chown -R ec2-user:ec2-user /home/ec2-user/chink
 
+cd /home/ec2-user/chink
+npm install
+
 # set files to 644 [except *.pl *.cgi *.sh]
 find /home/ec2-user/chink -type f -not -name ".pl" -not -name ".cgi" -not -name "*.sh" -print0 | xargs -0 chmod 0644
 
