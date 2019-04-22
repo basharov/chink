a1674bc1beec1b8845ce580877ff62b78197c186
Sergei Basharov <sergei@basharov.net>
2019-04-22 21:28:39 +0300

    permissions fixes

--- deploy/set-permissions.sh
+++ deploy/set-permissions.sh
@@ -2,9 +2,9 @@
 # Set ownership for all folders
 sudo chown -R ec2-user:ec2-user /home/ec2-user/chink
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 10.15.3
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+#. ~/.nvm/nvm.sh
+#nvm install 10.15.3
 
 cd /home/ec2-user/chink
 npm install
