ce25bdf49c898b0b4620700971ca6e403c3bd856
Sergei Basharov <sergei@basharov.net>
2019-04-22 21:08:45 +0300

    permissions fixes

--- deploy/set-permissions.sh
+++ deploy/set-permissions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Set ownership for all folders
-#chown -R ec2-user:ec2-user /home/ec2-user/chink
+sudo chown -R ec2-user:ec2-user /home/ec2-user/chink
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 . ~/.nvm/nvm.sh
@@ -10,7 +10,7 @@ cd /home/ec2-user/chink
 npm install
 
 # set files to 644 [except *.pl *.cgi *.sh]
-#find /home/ec2-user/chink -type f -not -name ".pl" -not -name ".cgi" -not -name "*.sh" -print0 | xargs -0 chmod 0644
+sudo find /home/ec2-user/chink -type f -not -name ".pl" -not -name ".cgi" -not -name "*.sh" -print0 | xargs -0 chmod 0644
 
 # set folders to 755
-#find /home/ec2-user/chink -type d -print0 | xargs -0 chmod 0755
+sudo find /home/ec2-user/chink -type d -print0 | xargs -0 chmod 0755
