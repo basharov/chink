efc9b1ba5bbde80b82ea6272c6ff77ea4e2be9ee
Sergei Basharov <sergei@basharov.net>
2019-04-23 12:27:46 +0300

    updated deps

--- deploy/set-permissions.sh
+++ deploy/set-permissions.sh
@@ -6,12 +6,12 @@ sudo chown -R ec2-user:ec2-user /home/ec2-user/chink
 #. ~/.nvm/nvm.sh
 #nvm install 10.15.3
 
-cd /home/ec2-user/chink
-#rm -rf node_modules
-npm install
-
 # set files to 644 [except *.pl *.cgi *.sh]
 sudo find /home/ec2-user/chink -type f -not -name ".pl" -not -name ".cgi" -not -name "*.sh" -print0 | xargs -0 chmod 0644
 
 # set folders to 755
 sudo find /home/ec2-user/chink -type d -print0 | xargs -0 chmod 0755
+
+cd /home/ec2-user/chink
+#rm -rf node_modules
+npm install
