e0531f6d2052442614481d8a24a044013140c6e4
Sergei Basharov <sergei@basharov.net>
2019-04-23 12:39:39 +0300

    hooks config

--- deploy/after-install.sh
+++ deploy/after-install.sh
@@ -2,16 +2,17 @@
 # Set ownership for all folders
 sudo chown -R ec2-user:ec2-user /home/ec2-user/chink
 
-#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-#. ~/.nvm/nvm.sh
-#nvm install 10.15.3
-
 # set files to 644 [except *.pl *.cgi *.sh]
 sudo find /home/ec2-user/chink -type f -not -name ".pl" -not -name ".cgi" -not -name "*.sh" -print0 | xargs -0 chmod 0644
 
 # set folders to 755
 sudo find /home/ec2-user/chink -type d -print0 | xargs -0 chmod 0755
 
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+#. ~/.nvm/nvm.sh
+#nvm install 10.15.3
+
+
 cd /home/ec2-user/chink
-#rm -rf node_modules
+
 npm install
--- deploy/install-deps.sh
+++
@@ -1,7 +0,0 @@
-#source /home/ec2-user/.bash_profile
-
-#sudo chown -R ec2-user:ec2-user /home/ec2-user/chink
-
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 10.15.3
